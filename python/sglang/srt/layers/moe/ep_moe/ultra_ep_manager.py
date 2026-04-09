497f9038b40b22484b204add29b798ee8bf16318
Xinming Wei <weixinming@pku.edu.cn>
2026-04-09 10:10:53 +0000

    feat: Support quota solver

--- python/sglang/srt/layers/moe/ep_moe/ultra_ep_manager.py
+++ python/sglang/srt/layers/moe/ep_moe/ultra_ep_manager.py
@@ -66,8 +66,6 @@ class UltraEPManager:
             is_train=False,
             explicitly_destroy=False,
             max_microbatches=1,
-            use_gpu_solver=True,
-            balance_threshold=0.0,
         )
 
         self.local_replica_weight_buffer: torch.Tensor = (
